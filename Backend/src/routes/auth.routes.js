484268274819c16b40af28876b68c80a155fd824
mishra-pankaj <immishrapankaj@gmail.com>
2026-05-12 23:32:11 +0530

    refactor auth login limiter with middleware

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,18 +1,10 @@
 const express = require("express")
 const  authControllers = require("../controllers/auth.contoller")
 const router = express.Router()
-const rateLimit = require("express-rate-limit")
-
-const loginLimiter = rateLimit({
-    windowMs: 1 * 60 * 1000, // 1 minute
-    max: 5,// limit each IP to 5 requests per windowMs
-    message: {
-        message: "Too many login attempts. Try again after 1 minute."
-    }
-})
+const loginLimit = require("../middlewares/loginlimiter")
 
 router.post("/register",authControllers.registerUser)
-router.post("/login", loginLimiter,authControllers.loginUser)
+router.post("/login", loginLimit,authControllers.loginUser)
 router.get("/logout", authControllers.logoutUser)
 
 
