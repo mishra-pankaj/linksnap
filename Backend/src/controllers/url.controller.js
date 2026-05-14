90bba25f088824f0f7813d8ae812df8337c852fe
mishra-pankaj <immishrapankaj@gmail.com>
2026-05-14 14:32:37 +0530

    url -analysis api done + bug fixes

--- Backend/src/controllers/url.controller.js
+++ Backend/src/controllers/url.controller.js
@@ -136,4 +136,159 @@ async function redirectURL(req,res){
     }   
 }
 
-module.exports = {shortenUrl,redirectURL}
+async function getAnalytics(req, res){
+    try{
+        const {shortId} = req.params
+
+        if(!shortId){
+            return res.status(400).json({
+                error: "Short Id is required",
+                code: "Missing Short id "
+            })
+        }
+        const url = await urlModel.findOne({shortId})
+
+        if(!url){
+            return res.status(404).json({
+                error:"Short link not found ",
+                code:" SHORT_LINK_NOT_FOUND"
+            })
+        }
+
+        if (!url.user || url.user.toString() !== req.user.id) {
+            return res.status(403).json({
+                error : "You do not have permission to view analystic for the link",
+                code: "FORBIDDEN"
+            })
+        }
+        //total-click
+        const totalClickResult = await clickModel.countDocuments({ shortId});
+
+        const totalClick = totalClickResult || 0;
+        //last 24 hr
+        const now = new Date()
+        const twentyFourhourAgo = new Date(now.getTime() -24 * 60* 60 * 1000);
+
+        const click24hResult = await clickModel.aggregate([
+        {
+            $match:{
+                shortId: shortId,
+                timestamp: {$gte: twentyFourhourAgo}
+            }
+        },
+        {$count: 'count'}   
+        ]);
+        const click24h = click24hResult[0]?.count || 0;
+        
+        const sevenDaysAgo = new Date(now.getTime() - 7 * 24 * 60 * 60 * 1000);
+ 
+        const clicks7dResult = await clickModel.aggregate([
+            {
+                $match: {
+                shortId: shortId,
+                timestamp: { $gte: sevenDaysAgo }
+                }
+            },
+            { $count: 'count' }
+        ]);
+        const clicks7d = clicks7dResult[0]?.count || 0;
+
+        const thirtyDaysAgo = new Date(now.getTime() - 30 * 24 * 60 * 60 * 1000);
+        const clicks30dResult = await clickModel.aggregate([
+            {
+                $match: {
+                shortId: shortId,
+                timestamp: { $gte: thirtyDaysAgo }
+                }
+            },
+            { $count: 'count' }
+        ]);
+        const clicks30d = clicks30dResult[0]?.count || 0;
+
+        //reference - 
+        const topReferrers = await clickModel.aggregate([
+           {$match:{shortId}},
+           {
+            $group:{
+                _id : '$referrer',
+                count: { $sum: 1}
+            }
+           },
+           {$sort: {count: -1}},
+           {$limit: 5},
+           {
+            $project: {
+                referrer: '$_id',
+                count: 1,
+                _id: 0
+            }
+           }
+        ])
+
+        //top-5 countries 
+        const topCountries = await clickModel.aggregate([
+            { $match: {shortId}},
+            {
+                $group: {
+                    _id: '$country',
+                    count: {$sum: 1}
+                }
+            },
+            {$sort : {count: -1}},
+            {$limit: 5},
+            {
+                $project: {
+                    country : '$_id',
+                    count: 1,
+                    _id:0,
+                }
+            }
+        ]);
+        //timeseries- click per day 
+        const timeseries = await clickModel.aggregate([
+            { $match: { shortId } },
+            {
+             $group: {
+                _id: {
+                    $dateToString: {
+                     format: '%Y-%m-%d',
+                     date: '$timestamp'
+                    }
+                },
+             clicks: { $sum: 1 }
+            }
+        },
+        { $sort: { _id: 1 } },
+        {
+         $project: {
+          date: '$_id',
+          clicks: 1,
+          _id: 0
+        }
+      }
+    ]);
+
+    //return analystic-
+    return res.status(200).json({
+        success: true,
+        shortId: shortId,
+        totalClick: totalClick,
+        click_24h: click24h,
+        click_7d: clicks7d,
+        clicks_30d: clicks30d,
+        topReferrers: topReferrers,
+        topCountries: topCountries,
+        timeseries: timeseries,
+        message: 'Analytics retrieved successfully'
+    });
+    }catch(err){
+        console.error("error in getAnalystic:",err);
+    }
+
+    return res.status(500).json({
+        error: "Failed to retrive Analystics",
+        code: "server_ error"
+    })
+}
+
+module.exports = {shortenUrl,redirectURL,getAnalytics}
--- Backend/src/routes/url.routes.js
+++ Backend/src/routes/url.routes.js
@@ -6,5 +6,5 @@ const authMiddleware = require("../middlewares/auth.middleware")
 
 router.post("/shorten",authMiddleware.optional,urlcontroller.shortenUrl)
 router.get("/:shortId",authMiddleware.optional,urlcontroller.redirectURL)
-
+router.get("/analytics/:shortId", authMiddleware.required, urlcontroller.getAnalytics)
 module.exports = router
